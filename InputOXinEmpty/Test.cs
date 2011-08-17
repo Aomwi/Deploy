590c257abc85a0039078c7b8b8d72359b1ccea24
spiderart <spider_art@hotmail.com>
2011-08-17 15:26:22 +0700

    ignore bin and obj folders + add Test.cs

---
+++ InputOXinEmpty/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace InputOXinEmpty
+{
+    [TestFixture]
+    class Test
+    {
+        [Test]
+
+        public void TestInput() 
+        {
+            Assert.AreEqual('O', Input.Evaluate("A"));
+            
+        }
+    }
+}
