17d2b4b43a02db8fb37c5f61f908e059076260f3
k-chao <krachao4444@hotmail.com>
2011-08-19 13:39:49 +0700

    Update for user test

---
+++ TicTacToe_NewComplete/ClassForTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace TicTacToe
+{
+
+    class ClassForTest
+    {
+        public static string name;
+
+        public static string ShowName(string str)
+        {
+            return str;
+        }
+
+        public static void SetName(string n)
+        {
+            name = n;
+        }
+
+    }
+}
